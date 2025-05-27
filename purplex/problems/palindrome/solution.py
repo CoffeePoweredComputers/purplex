4e0657687f8ada0abe75cc100675e4f4b44f5668
David Smith <davidsmith@hamiltonfour.tech>
2025-05-26 22:17:22 -0500

    removed old test case stuff

--- purplex/problems/palindrome/solution.py
+++
@@ -1,6 +0,0 @@
-def foo(s):
-    for i in range(len(s)//2):
-        if s[i] != s[-1-i]:
-            return False
-
-
--- purplex/problems/reverse_string/solution.py
+++
@@ -1,7 +0,0 @@
-def foo(x):
-    y = ""
-    for i in range(len(x)-1, -1, -1):
-        y += x[i]
-    return y
-
-print(foo("hello"))
